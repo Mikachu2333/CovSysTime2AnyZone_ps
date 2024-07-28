51ac19cd1db7b31e8bdbe3dd5971f6af69057bad
Mikachu2333 <mikachu.23333@zohomail.com>
2024-07-29 04:30:55 +0930

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,16 @@
+# Which time zone you need
+$NeedZone = 8
+# Which format you'd like to use for output
+$OutputFormat = "%Y-%m-%d_%H:%M:%S"
+#
+#
+# Get time now as seconds with zone
+$NowTime = [UInt32](Get-Date -UFormat "%s")
+# Get zone as int (never use uint for this)
+$Zone = [Int16](Get-Date -UFormat "%Z").ToString()
+# Switch to Zone you need
+$NeedTime = ($NeedZone - $Zone) * 60 * 60
+$Result = $NowTime + $NeedTime
+# Format it
+$Formated = (Get-Date -UnixTimeSeconds $Result -UFormat $OutputFormat).ToString()
+$Formated
